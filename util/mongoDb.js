704f17cf9cc212ea254aa69bcb04084083192e3d
Lucas Lima <lucas.lima@mundiale.com.br>
2022-07-27 21:57:54 -0300

    fix: concertando classe de bd

--- util/mongoDb.js
+++ util/mongoDb.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
 class MongoDB {
-    static connect(test = false) {
-        const URI = MongoDB.getUri(test);
-
-        return mongoose.connect(URI);
+    static async connect() {
+        return await mongoose.connect('mongodb://localhost:27017/loja-virtual');
     }
 
-    static  closeConnection() {
-        mongoose.connection.close();
+    static async connectTest() {
+        return await mongoose.connect('mongodb://localhost:27017/loja-virtual-test', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }
+
+    static async closeConnectionDropDatabase() {
+        await mongoose.connection.db.dropDatabase();
+        await mongoose.disconnect();
     }
 
     static getUri(test) {
         if (test) {
-            return  'mongodb://localhost:27017/loja-virtual-test';
+            return 'mongodb://localhost:27017/loja-virtual-test';
         } else {
             return 'mongodb://localhost:27017/loja-virtual';
         }
